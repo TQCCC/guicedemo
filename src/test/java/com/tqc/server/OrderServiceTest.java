b15e061f38bbdd90fbdd3236e9919eacfbdc79f0
TQCCC <tqcccc@sina.com>
2017-12-24 12:28:53 +0800

    覆盖绑定：override

--- src/test/java/com/tqc/server/OrderServiceTest.java
+++ src/test/java/com/tqc/server/OrderServiceTest.java
@@ -1,15 +1,34 @@
 package com.tqc.server;
 
+import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
+import com.google.inject.util.Modules;
+import com.tqc.server.impl.PriceServiceImpl;
 import com.tqc.server.module.ServerModule;
 import org.junit.Before;
 import org.junit.Test;
 
 import javax.inject.Inject;
+import java.util.Set;
 
 /**
  * Created by tangqingchang on 2017/12/23.
  */
+
+class PriceServiceMock extends PriceServiceImpl {
+
+	@Inject
+	public PriceServiceMock(Set<String> supportedCurrencies) {
+		super(supportedCurrencies);
+	}
+
+	@Override
+	public long getPrice(long orderId) {
+		return 666L;
+	}
+}
+
+
 public class OrderServiceTest {
 
 	@Inject
@@ -21,7 +40,18 @@ public class OrderServiceTest {
 
 	@Before
 	public void setUp() {
-		Guice.createInjector(new ServerModule()).injectMembers(this);
+		Guice.createInjector(
+				Modules.override(new ServerModule())
+						.with(
+								new AbstractModule() {
+									@Override
+									protected void configure() {
+										bind(PriceService.class).to(PriceServiceMock.class);
+									}
+								}
+						)
+		)
+				.injectMembers(this);
 	}
 
 	@Test
