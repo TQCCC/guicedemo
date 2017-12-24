9dbd00c452ebca7925e683e52f39d497eedc19e5
TQCCC <tqcccc@sina.com>
2017-12-24 21:00:36 +0800

    初始化guicedemospring

---
+++ guicedemospring/src/main/java/com/tqc/guicedemospring/GuiceDemoSpringApplication.java
@@ -0,0 +1,12 @@
+package com.tqc.guicedemospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiceDemoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuiceDemoSpringApplication.class, args);
+	}
+}
